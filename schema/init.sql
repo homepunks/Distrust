84c93452ea1730f15856868a565e26beaedff25b
homepunks <wispenjoyer@gmail.com>
2026-02-14 17:17:15 +0500

    Update schema

--- schema/init.sql
+++ schema/init.sql
@@ -1,4 +1,8 @@
-CREATE TABLE IF NOT EXISTS resources (
-    uid TEXT PRIMARY KEY,
-    content TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS pastes (
+	id TEXT PRIMARY KEY,
+    	content BLOB NOT NULL,
+    	content_type TEXT NOT NULL DEFAULT 'text/plain',
+	size INTEGER NOT NULL,
+	created_at INTEGER NOT NULL,
+	view_count INTEGER DEFAULT 0
 );
