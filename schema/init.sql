21f20d96f584cfd62fd169433f5e8bc86e92f973
homepunks <wispenjoyer@gmail.com>
2026-02-20 14:53:38 +0500

    Align schema

--- schema/init.sql
+++ schema/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS pastes (
 	id TEXT PRIMARY KEY,
-    	content BLOB NOT NULL,
-    	content_type TEXT NOT NULL DEFAULT 'text/plain',
+  content BLOB NOT NULL,
+  content_type TEXT NOT NULL DEFAULT 'text/plain',
 	size INTEGER NOT NULL,
 	created_at INTEGER NOT NULL,
 	view_count INTEGER DEFAULT 0
